facda241e71eafcbedd9a949c3f38d667a3f8c7a
w91379137 <w913791379137@gmail.com>
2024-06-26 03:00:28 +0800

    [feat] 完成 worker 測試

--- backend/public/javascripts/worker.js
+++ backend/public/javascripts/worker.js
@@ -1,11 +1,34 @@
 
 console.log("Worker script loaded");
 
+var id = 0;
+
+const doWork = () => {
+    // console.log("doWork");
+    fetch('/log?page=worker')
+    .then((response) => {
+        console.log(response); 
+    })
+    .catch((error) => {
+        console.log(`Error: ${error}`);
+    })
+    // postMessage("doWork");
+}
+
 // 進入點？
 onmessage = function (message) {
     console.log("Message received from main script", message);
     const data = message.data;
-    var result = "Result: " + data[0] + data[1];
+
+    var result = "Result: " + data.join(" ");
     console.log("Posting message back to main script");
     postMessage(result);
+
+    if (data.includes("start")) {
+        id = setInterval(doWork, 5000);
+    }
+    if (data.includes("stop")) {
+        clearInterval(id);
+        this.close();
+    }
 };
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
+  res.render('worker_index.html');
 });
 
-router.get('/sub', function(req, res, next) {
-  res.render('sub.html');
+router.get('/index', function(req, res, next) {
+  res.render('index.html');
 });
 
 router.get('/worker_index', function(req, res, next) {
   res.render('worker_index.html');
+});
+
+router.get('/sub', function(req, res, next) {
+  res.render('sub.html');
 });
 
 module.exports = router;
