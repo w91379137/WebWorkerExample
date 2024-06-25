7668d9c91f6decdb3ff5b257a3cb4d3445e99fba
w91379137 <w913791379137@gmail.com>
2024-06-25 21:31:05 +0800

    [feat] 新增子頁

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { join } = require('path')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -14,6 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.set("views", join(__dirname, "public"));
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
