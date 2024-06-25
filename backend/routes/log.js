a84c32dc3a80ecb87e0687fe8dc22930c994bc4c
w91379137 <w913791379137@gmail.com>
2024-06-25 20:21:49 +0800

    [feat] 新增 log 寫檔機制

---
+++ backend/routes/log.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'a'});
+
+var log = (message) => {
+    const date = (new Date()).toISOString();
+    log_file.write(util.format(date + "\n" + message) + '\n');
+}
+
+const handler = function(req, res, next) {
+    // console.log('log request', req.method, req.url);
+    let param = {}
+    if (req.method === 'GET') {
+        param = req.query;
+    }
+    if (req.method === 'POST') {
+        param = req.body;
+    }
+    log(JSON.stringify(param, null, 2));
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(param, null, 2));
+}
+
+router.get('/', handler);
+router.post('/', handler);
+
+module.exports = router;
