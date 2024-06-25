5031b5f140c4ad85cd3cb1be014ccfe262e12a3c
w91379137 <w913791379137@gmail.com>
2024-06-26 02:07:39 +0800

    [feat] 修改 worker log

--- backend/public/javascripts/worker.js
+++ backend/public/javascripts/worker.js
@@ -1,8 +1,11 @@
 
-onmessage = function (e) {
-    console.log("Message received from main script", e);
-    const data = e.data;
-    var workerResult = "Result: " + data[0] + data[1];
+console.log("Worker script loaded");
+
+// 進入點？
+onmessage = function (message) {
+    console.log("Message received from main script", message);
+    const data = message.data;
+    var result = "Result: " + data[0] + data[1];
     console.log("Posting message back to main script");
-    postMessage(workerResult);
+    postMessage(result);
 };
